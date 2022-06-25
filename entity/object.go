022ebb7339d1e4d26be33666df67bd5b71fef7ad
Mobyus <theonlymoby@protonmail.com>
2022-06-25 23:43:08 +0300

    object template

---
+++ entity/object.go
@@ -0,0 +1,20 @@
+package entity
+
+type Object struct {
+	X      float64
+	Y      float64
+	VX     float64
+	VY     float64
+	Width  int
+	Height int
+}
+
+func (o *Object) CollideWith(n *Object) bool {
+	if o.X+float64(o.Width) >= n.X &&
+		o.X <= n.X+float64(n.Width) &&
+		o.Y+float64(o.Height) >= n.Y &&
+		o.Y <= n.Y+float64(n.Height) {
+		return true
+	}
+	return false
+}
